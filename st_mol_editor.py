7236063843492f9e90f55febc74ad2360ce22b2d
Yew Mun <yipy0005@gmail.com>
2023-04-14 17:33:41 +0800

    add streamlit app file

---
+++ st_mol_editor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python
+
+import streamlit as st
+from streamlit_ketcher import st_ketcher
+
+molecule = st.text_input("Molecule", "CCO")
+smile_code = st_ketcher(molecule)
+st.markdown(f"Smile code: ``{smile_code}``")
